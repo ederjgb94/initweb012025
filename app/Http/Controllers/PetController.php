c9041c7992e8f0f9fda9718a1856a10d5d0eb302
“ederjgb94” <ederjbg94@gmail.com>
2025-01-27 13:00:32 -0600

    Add Pet model, controller, migration, and factory; update DatabaseSeeder and routes

---
+++ app/Http/Controllers/PetController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Pet;
+use Illuminate\Http\Request;
+
+class PetController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Pet::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        // $pet = new Pet();
+        // $pet->name = $request->name;
+        // $pet->age = $request->age;
+        // $pet->save();
+
+        //ahora hazlo de asignacion masiva
+        $pet = Pet::create($request->all());
+
+        return $pet;
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Pet $pet)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Pet $pet)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Pet $pet)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,19 +3,27 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
 
 
-Route::get('/hello1', function () {
-    return response()->json(['message' => 'Hello World1!']);
-});
+// Route::get('/hello1', function () {
+//     return response()->json(['message' => 'Hello World1!']);
+// });
 
-Route::post('/hello2', function (Request $request) {
-    $edad = $request['edad'];
-    $costo = $request['costo'];
-    $suma = $edad + $costo;
+// Route::post('/hello2', function (Request $request) {
+//     $edad = $request['edad'];
+//     $costo = $request['costo'];
+//     $suma = $edad + $costo;
     
-    return response()->json(['suma' => $suma]);
-});
+//     return response()->json(['suma' => $suma]);
+// });
+
+Route::apiResource('pets', 'App\Http\Controllers\PetController');
+
+// Route::get('/pets', 'App\Http\Controllers\PetController@index');
+// Route::post('/pets', 'App\Http\Controllers\PetController@store');
+// Route::get('/pets/{pet}', 'App\Http\Controllers\PetController@show');
+// Route::put('/pets/{pet}', 'App\Http\Controllers\PetController@update');
+// Route::delete('/pets/{pet}', 'App\Http\Controllers\PetController@destroy');
